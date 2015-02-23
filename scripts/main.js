f9f31494df8b9ed3ca044e2926af06b2ba710bdd
Daniel Edwards <cdaniel.edwards@gmail.com>
2015-02-23 17:38:08 -0500

    Beginning to construct the Models and Collections

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,57 @@
 
   window.App = window.App || {};
 
+  //
+  // Dumb Data
+  //
+
   var dumData = {
     url: 'http://en.wikipedia.org/wiki/Llama',
     title: 'Lama Wiki',
     description: 'A great resource for llama research.',
     tag: 'llamas'
   };
+
+  var dumTag = {tag: 'whatever'};
+
+  //
+  // Models
+  //
+
+  var BookmarkModel = Backbone.Model.extend({
+    defaults: {
+      url: '',
+      title: '',
+      description '',
+      tag: '',
+    }
+
+    var bookmarks =
+      {
+        url: 'http://en.wikipedia.org/wiki/Llama',
+        title: 'Lama Wiki',
+        description: 'A great resource for llama research.',
+        tag: 'llamas'
+      },
+      {
+        url: 'http://en.wikipedia.org/wiki/Giant_panda',
+        title: 'Panda Wiki',
+        description: 'A great resource for Panda research.',
+        tag: 'pandas'
+      },
+      {
+        url: 'http://en.wikipedia.org/wiki/Sloth',
+        title: 'Sloth Wiki',
+        description: 'A great resource for sloth research.',
+        tag: 'sloths'
+      };
+  });
+
+
+
+  //
+  // Views
+  //
 
   var CountView = Backbone.View.extend({
     template : _.template($('#count-template').text()),
@@ -50,6 +95,22 @@
     }
   });
 
+  var TagItemView = Backbone.View.extend({
+    tagName: 'li',
+    template : _.template($('#tag-item-template').text()),
+
+    render: function(){
+      $('.js-tags-ul').append(this.el);
+      this.$el.html(this.template(dumTag));
+      return this;
+    }
+
+  });
+
+  //
+  // Router
+  //
+
   var AppRouter = Backbone.Router.extend({
     routes: {
       '': 'index'
@@ -60,6 +121,7 @@
       this.bookmarksListView = new BookmarksListView({el: '.js-bookmarks-ul'});
       this.tagsListView = new TagsListView({el: '.js-tags'});
       this.bookmarksItemView = new BookmarksItemView();
+      this.tagItemView = new TagItemView();
     },
 
     index: function(){
@@ -67,6 +129,7 @@
       this.bookmarksListView.render();
       this.bookmarksItemView.render();
       this.tagsListView.render();
+      this.tagItemView.render();
     },
 
   });
