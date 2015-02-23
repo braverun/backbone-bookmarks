24630d2ff49929c9bff5d82ae488673d91388c7e
Daniel Edwards <cdaniel.edwards@gmail.com>
2015-02-23 17:10:03 -0500

    Appended the li to the ul in the BookmarksItemView

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,13 @@
 
   window.App = window.App || {};
 
+  var dumData = {
+    url: 'http://en.wikipedia.org/wiki/Llama',
+    title: 'Lama Wiki',
+    description: 'A great resource for llama research.',
+    tag: 'llamas'
+  };
+
   var CountView = Backbone.View.extend({
     template : _.template($('#count-template').text()),
 
@@ -21,6 +28,17 @@
     }
   });
 
+  var BookmarksItemView = Backbone.View.extend({
+    tagName: 'li',
+    template : _.template($('#bookmarks-item-template').text()),
+
+    render: function(){
+      $('.js-bookmarks-ul').append(this.el);
+      this.$el.html(this.template(dumData));
+      return this;
+    }
+
+  });
 
 
   var TagsListView = Backbone.View.extend({
@@ -41,11 +59,13 @@
       this.countView = new CountView({el: '.js-count'});
       this.bookmarksListView = new BookmarksListView({el: '.js-bookmarks-ul'});
       this.tagsListView = new TagsListView({el: '.js-tags'});
+      this.bookmarksItemView = new BookmarksItemView();
     },
 
     index: function(){
       this.countView.render();
       this.bookmarksListView.render();
+      this.bookmarksItemView.render();
       this.tagsListView.render();
     },
 
