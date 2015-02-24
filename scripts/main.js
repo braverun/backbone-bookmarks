5bfc6bdcb495eb6f2392d46c5f73b26d2c2edecd
Beth Hoskins <bhoskins@charter.net>
2015-02-24 07:37:05 -0500

    Group project with Weston and Dan

--- scripts/main.js
+++ scripts/main.js
@@ -92,7 +92,7 @@
 
 
     render: function(){
-      this.$el.html(this.template());
+      this.$el.html(this.template(this.collection));
       return this;
     }
   });
@@ -141,7 +141,24 @@
     },
 
     initialize: function(){
-      this.bookmarksCollection = new BookmarksCollection();
+      this.bookmarksCollection = new BookmarksCollection({
+        url: 'http://en.wikipedia.org/wiki/Llama',
+        title: 'Lama Wiki',
+        description: 'A great resource for llama research.',
+        tag: 'llamas'
+      },
+      {
+        url: 'http://en.wikipedia.org/wiki/Giant_panda',
+        title: 'Panda Wiki',
+        description: 'A great resource for Panda research.',
+        tag: 'pandas'
+      },
+      {
+        url: 'http://en.wikipedia.org/wiki/Sloth',
+        title: 'Sloth Wiki',
+        description: 'A great resource for sloth research.',
+        tag: 'sloths'
+      });
       this.countView = new CountView({el: '.js-count'});
       this.bookmarksListView = new BookmarksListView({
           el: '.js-bookmarks-ul',
